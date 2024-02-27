f827a645ef0b4557eecdcef286686e4f2082b339
Kerrynkiria <145858804+Kerrynkiria@users.noreply.github.com>
2024-02-27 23:54:15 +0300

    user_input.py

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input("What is your name: ")
+age=int(input("What is your age: "))
+location=input("What is your location: ")
+print(f"Hello {name}, you are {age} years and you live in {location}")
